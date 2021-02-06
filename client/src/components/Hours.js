c964a3f9752ea4c612a74104faef0c1b1f4fa62c
Corina <cmangione15@yahoo.com>
2021-02-05 22:30:17 -0500

    styling

--- client/src/components/Hours.js
+++ client/src/components/Hours.js
@@ -5,9 +5,29 @@ import React, { Component } from 'react';
 class Hours extends Component {
     render() {
         return (
-            <div>
+            <div style = {{
+                backgroundColor: 'chocolate',
+                position:'fixed',
+               
+                left: '0',
+                width:'100%',
+                height:'100%'
+               
+
+
+            }}>
+            <div style = {{
+                backgroundColor: 'burlywood',
+                width: '50%',
+                justifyContent:'center',
+                alignItems:'center',
+                margin: '0 auto'
+                
+
+            }}>
+            
                 <h1>Coffee Shop Community Library</h1>
-           <img src='http://images.amazon.com/images/P/1552041778.01.THUMBZZZ.jpg'></img>
+           <img src='http://images.amazon.com/images/P/1552041778.01.MZZZZZZZ.jpg'></img>
             <table>
 <tr><th>Sunday</th><td>Closed</td></tr>
 <tr><th>Monday</th><td>9am - 5pm</td></tr>
@@ -15,6 +35,7 @@ class Hours extends Component {
 <tr><th>Wednesday</th><td>9am - 5pm</td></tr>
 <tr><th>Thursday</th><td>9am - 5pm</td></tr>
 </table>
+         </div>
          </div>
         );
     }
