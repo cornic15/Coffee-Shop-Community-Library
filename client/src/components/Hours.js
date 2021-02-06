e5e5f4ebdf879627e423cfe20840a8723f506381
Corina <cmangione15@yahoo.com>
2021-02-05 22:13:19 -0500

    add image and title to home page

--- client/src/components/Hours.js
+++ client/src/components/Hours.js
@@ -5,7 +5,9 @@ import React, { Component } from 'react';
 class Hours extends Component {
     render() {
         return (
-           
+            <div>
+                <h1>Coffee Shop Community Library</h1>
+           <img src='http://images.amazon.com/images/P/1552041778.01.THUMBZZZ.jpg'></img>
             <table>
 <tr><th>Sunday</th><td>Closed</td></tr>
 <tr><th>Monday</th><td>9am - 5pm</td></tr>
@@ -13,7 +15,7 @@ class Hours extends Component {
 <tr><th>Wednesday</th><td>9am - 5pm</td></tr>
 <tr><th>Thursday</th><td>9am - 5pm</td></tr>
 </table>
-         
+         </div>
         );
     }
 }
